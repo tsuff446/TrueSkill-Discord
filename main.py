6846842e231de39cc2d54cfd698998bb0752e9f5
Thomas Suff <36467286+tsuff446@users.noreply.github.com>
2021-01-25 22:54:58 -0600

    added todo

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from src.player import Player
 from src.trueskill_helpers import check_fair, report_match
 import numpy as np
 from src.spreadsheet_helpers import players_to_spreadsheet, spreadsheet_to_players
+import matplotlib.pyplot as plt
 
 players = []
-num_players = 10
-num_matches = 1000
+num_players = 100
+num_matches = 100000
 
 for i in range(num_players):
-    players.append(Player(str(i), "hi"))
+    players.append(Player(str(i), str(i)))
 
 for match in range(num_matches):
     participants = np.random.choice(players, 2, replace=False)
@@ -20,6 +21,5 @@ for match in range(num_matches):
 for player in players:
     player.skill_report()
 
-players_to_spreadsheet("backups/backup.csv", players)
-new_players = spreadsheet_to_players('backups/backup.csv')
-
+plt.hist([player.rating for player in players])
+plt.show()
