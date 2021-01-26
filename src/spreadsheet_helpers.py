4ec7eeeaece208168d6d45bf5d856acb60d757fd
Thomas Suff <36467286+tsuff446@users.noreply.github.com>
2021-01-25 23:28:23 -0600

    improved readability

--- src/spreadsheet_helpers.py
+++ src/spreadsheet_helpers.py
@@ -1,23 +1,27 @@
 from src.player import Player
 
+#helper converts Player() to row
 def player_to_row(player):
     return (str(player.discord_id), str(player.discord_alias), str(player.rating), str(player.mu), str(player.sigma), str(player.wins), str(player.losses))
 
-def players_to_spreadsheet(name, players):
-    f = open(name, 'w')
+#converts list of players to csv
+def players_to_spreadsheet(path, players):
+    f = open(path, 'w')
     #first line:
     f.write("Discord ID, Discord Name, Rating, Mu, Sigma, Wins, Losses \n")
     for player in players:
         f.write(','.join(player_to_row(player)) + "\n")
     f.close()
 
+#helper converts csv row to player object
 def row_to_player(row):
     return Player(int(row[0]), str(row[1]), mu=float(row[3]), sigma=float(row[4]), wins=int(row[5]), losses=int(row[6]))
 
-def spreadsheet_to_players(name):
+#constructs player list from csv
+def spreadsheet_to_players(path):
     players = []
     try:
-        f = open(name, 'r')
+        f = open(path, 'r')
         #skip first line
         line = f.readline()
         line = f.readline()
